85164c97938bde4dcb85faa834adb76974cc3b27
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-02 21:30:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -84,5 +84,4 @@ function placeOrder(cardNumber) {
  console.log(`Sorry, we don't have a credit card on file for you.`)
  else 
  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
- delete(cart)
 }
